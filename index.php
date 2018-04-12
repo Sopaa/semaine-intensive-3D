a8d019f4cfd85663ac213251020f4fbc0e1738ee
Vnk <vnk@MacBook-Pro-de-Vnk.local>
2018-04-12 21:31:32 +0200

    erpoigjeo

--- index.php
+++ index.php
@@ -28,20 +28,151 @@
   </section>
 
 
-  <section class="description">
-    <div class="description__container">
-      <div class="description__content">
-        <h3 class="description__h3">Descriptif</h3>
-        <p class="description__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid et nisi nesciunt enim consequatur optio impedit eveniet explicabo provident eum!</p>
+
+
+<div class="section__container">
+
+<div class="section__one">
+  <div class="description__text">
+      <div class="description__text-1">
+        <p>Imagines-toi dans un monde où tout le monde possède son propre drones <span class="combi">personnalisables</span> à ses <span class="combi">envies</span>, ses <span class="combi">goûts</span> et surtout à ses <span class="combi">besoins</span> !</p>
       </div>
 
-      <img class="description__img img-1" src="../img/nat-1.jpg" alt="Drone 1">
-      <img class="description__img img-2" src="../img/nat-2.jpg" alt="Drone 2">
-      <div class="description__container-link">
-        <a class="description__link" href="">Start Here</a>
+      <div class="description__text-2">
+        <p>Plus de <span class="combi">150 combinaisons</span> possible selon ton imagination à l’image de tes goûts !</p>
       </div>
+
+      <div class="description__text-3">
+        <p>Et ce n’est pas tout ..!</p>
+      </div>
+
+      <div class="description__text-4">
+        <p>Aujourd’hui avec <span class="edward">Edward</span>, le drone devient fonctionnel !</p>
+      </div>
+  </div>
+</div>
+
+<div class="section__two">
+  <div class="description__part2">
+
+      <div class="description__part2-text">
+        <p>Imagines que ton drone effectue chaque tâches ménagère à ta place, qu’il puisse t’apporter des objets ou même qu’il soit en capacité de porter tes courses ! Même dans ta vie professionnelle il sera ton meilleur ami ! </p>
+      </div>
+
+  </div>
+</div>
+
+
+<div class="section__three">
+  <div class="description__part3">
+
+      <div class="description__part3-text">
+        <p>Un far a été intégré pour l’obscurité afin d’assurer la <span class="combi">surveillance</span> de votre maison, un <span class="combi">amplificateur de réseaux mobile</span>, un système de <span class="combi">"pigeon-voyageur"</span> et tout autres sorte d’équipements vont être ajoutés au cours des prochains mois. </br></br>
+          Tu sera étonné du gain de temps et d’énergie que tu vas pouvoir acquérir avec ton drone <span class="combi">multifonctions</span>.</p>
+      </div>
+
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="section__form">
+
+<div class="section__h4">
+    <h4 class="form__h4">Tu n'es pas convaincu à 100% ?</h4>
+    <h4 class="form__h4">Prends <span class="combi">rendez-vous</span> pour tester un drone !</h4>
+</div>
+
+
+<div class="form__group">
+
+  <div class="form__nom">
+    <label class="form__label" for="name">Nom</label>
+    <input class="form__input" type="text" name="name" id="name">
+  </div>
+
+  <div class="form__prenom">
+    <label class="form__label" for="name">Prénom</label>
+    <input class="form__input" type="text" name="name" id="name">
+  </div>
+
+  <div class="form__email">
+      <label for="email" class="form__label">Adresse Email</label>
+      <input type="email" class="form__input" placeholder="" id="email" required>
+  </div>
+
+
+</div>
+
+
+<div class="form__send">
+  <div class="form__send2">
+    <label for="text" class="form__label">Des informations supplémentaires?</label>
+  </div>
+    <input type="text" class="form__input send" placeholder="" id="email" required>
+
+    <div class="btn__container">
+        <a class="btn__link link-blue" href="">Valider</a>
     </div>
-  </section>
+</div>
+
+</div>
+
+
+  <!-- <footer class="footer">
+    <div class="footer__container">
+      <div class="contact">
+        <h4 class="footer__h4">Pour mieux nous connaître</h4>
+          <ul class="footer__list">
+            <li class="contact__list-item">À propos d'EDWARD</li>
+            <li class="contact__list-item">Carrières</li>
+            <li class="contact__list-item">Ensemble</li>
+          </ul>
+
+      </div>
+
+      <div class="shop__info">
+       <h4 class="footer__h4">Vos services</h4>
+       <ul class="footer__list">
+            <li class="shop__list-item">Expédia par EDWARD</li>
+            <li class="shop__list-item">Faire livrer votre drone</li>
+            <li class="shop__list-item">Cartes de paiement</li>
+            <li class="shop__list-item">Faire livrer votre drone</li>
+          </ul>
+      </div>
+
+      <div class="news">
+       <h4 class="footer__h4">Besoin d'aide?</h4>
+       <ul class="news__list">
+            <li class="news__list-item">Visualiser ou suivre vos commandes</li>
+            <li class="news__list-item">Tarifs et options de livraison</li>
+            <li class="news__list-item">Retours</li>
+          </ul>
+      </div>
+
+      <div class="copyright">
+        Copyright © 2018 notre site. All Rights Reserved.
+        <div class="socialimg">
+          <ul class="social__list">
+            <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/fb.png" alt=""></a></li>
+            <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/twitter.png" alt=""></a></li>
+            <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/snap.png" alt=""></a></li>
+            <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/insta.png" alt=""></a></li>
+          </ul>
+        </div>
+     </div>
+
+    </div>
+
+  </footer> -->
+
+  <div class="footerimg">
+
+  </div>
+
+
   <script src="js/main.js"></script>
 </body>
 
