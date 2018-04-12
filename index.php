858ae0899f343e32206f3c8a713062403a0e9438
Vnk <vnk@MacBook-Pro-de-Vnk.local>
2018-04-12 21:33:50 +0200

    index

--- index.php
+++ index.php
@@ -4,75 +4,72 @@
 <head>
   <meta charset="utf-8">
   <title>pas ptdr</title>
-  <link rel="stylesheet" href="assets/css/reset.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
   <header class="header">
-    <h1 class="header-title">Site.fr</h1>
+    <h1 class="header-title">Edward</h1>
     <nav class="nav">
       <ul class="nav-listContainer">
-        <li class="nav-listItem"><a href="index.php">Acceuil</a></li>
-        <li class="nav-listItem"><a href="">Drones</a></li>
-        <li class="nav-listItem"><a href="">Contact</a></li>
-        <li class="nav-listItem"><a href="cart.php"><img class="header-cart" src="assets/img/cart.png" alt="cart"></a></li>
+        <li class="nav-listItem"><a class="nav-listLink" href="index.php">Acceuil</a></li>
+        <li class="nav-listItem"><a class="nav-listLink" href="">Drones</a></li>
+        <li class="nav-listItem"><a class="nav-listLink" href="">Contact</a></li>
+        <li class="nav-listItem"><a class="nav-listLink" href="cart.php"><img class="header-cart" src="../img/panier.png" alt="cart"></a></li>
       </ul>
     </nav>
   </header>
   <section class="start">
-    <img src="assets/img/drone1.png" class="start-imgCarrousel" alt="exemple drone">
-    <img style="" src="assets/img/drone2.png" class="start-imgCarrousel" alt="exemple drone">
-    <img style="" src="assets/img/drone3.png" class="start-imgCarrousel" alt="exemple drone">
+    <img src="../img/homepage.png" class="start-imgCarrousel" alt="exemple drone">
+    <img style="" src="../img/homepage.png" class="start-imgCarrousel" alt="exemple drone">
+    <img style="" src="../img/homepage.png" class="start-imgCarrousel" alt="exemple drone">
+    <h3 class="header__h3">EDWARD,  le drone qui vous assiste au quotidien.</h3>
     <a class="start-button" href="configurator.php">Configure ton drone</a>
   </section>
 
-
-
-
 <div class="section__container">
 
-<div class="section__one">
-  <div class="description__text">
-      <div class="description__text-1">
-        <p>Imagines-toi dans un monde où tout le monde possède son propre drones <span class="combi">personnalisables</span> à ses <span class="combi">envies</span>, ses <span class="combi">goûts</span> et surtout à ses <span class="combi">besoins</span> !</p>
-      </div>
+  <div class="section__one">
+    <div class="description__text">
+        <div class="description__text-1">
+          <p>Imagines-toi dans un monde où tout le monde possède son propre drones <span class="combi">personnalisables</span> à ses <span class="combi">envies</span>, ses <span class="combi">goûts</span> et surtout à ses <span class="combi">besoins</span> !</p>
+        </div>
 
-      <div class="description__text-2">
-        <p>Plus de <span class="combi">150 combinaisons</span> possible selon ton imagination à l’image de tes goûts !</p>
-      </div>
+        <div class="description__text-2">
+          <p>Plus de <span class="combi">150 combinaisons</span> possible selon ton imagination à l’image de tes goûts !</p>
+        </div>
 
-      <div class="description__text-3">
-        <p>Et ce n’est pas tout ..!</p>
-      </div>
+        <div class="description__text-3">
+          <p>Et ce n’est pas tout ..!</p>
+        </div>
 
-      <div class="description__text-4">
-        <p>Aujourd’hui avec <span class="edward">Edward</span>, le drone devient fonctionnel !</p>
-      </div>
+        <div class="description__text-4">
+          <p>Aujourd’hui avec <span class="edward">Edward</span>, le drone devient fonctionnel !</p>
+        </div>
+    </div>
   </div>
-</div>
 
-<div class="section__two">
-  <div class="description__part2">
+  <div class="section__two">
+    <div class="description__part2">
 
-      <div class="description__part2-text">
-        <p>Imagines que ton drone effectue chaque tâches ménagère à ta place, qu’il puisse t’apporter des objets ou même qu’il soit en capacité de porter tes courses ! Même dans ta vie professionnelle il sera ton meilleur ami ! </p>
-      </div>
+        <div class="description__part2-text">
+          <p>Imagines que ton drone effectue chaque tâches ménagère à ta place, qu’il puisse t’apporter des objets ou même qu’il soit en capacité de porter tes courses ! Même dans ta vie professionnelle il sera ton meilleur ami ! </p>
+        </div>
 
+    </div>
   </div>
-</div>
 
 
-<div class="section__three">
-  <div class="description__part3">
+  <div class="section__three">
+    <div class="description__part3">
 
-      <div class="description__part3-text">
-        <p>Un far a été intégré pour l’obscurité afin d’assurer la <span class="combi">surveillance</span> de votre maison, un <span class="combi">amplificateur de réseaux mobile</span>, un système de <span class="combi">"pigeon-voyageur"</span> et tout autres sorte d’équipements vont être ajoutés au cours des prochains mois. </br></br>
-          Tu sera étonné du gain de temps et d’énergie que tu vas pouvoir acquérir avec ton drone <span class="combi">multifonctions</span>.</p>
-      </div>
+        <div class="description__part3-text">
+          <p>Un far a été intégré pour l’obscurité afin d’assurer la <span class="combi">surveillance</span> de votre maison, un <span class="combi">amplificateur de réseaux mobile</span>, un système de <span class="combi">"pigeon-voyageur"</span> et tout autres sorte d’équipements vont être ajoutés au cours des prochains mois. </br></br>
+            Tu sera étonné du gain de temps et d’énergie que tu vas pouvoir acquérir avec ton drone <span class="combi">multifonctions</span>.</p>
+        </div>
 
+    </div>
   </div>
-</div>
 
 
 </div>
@@ -80,81 +77,161 @@
 
 <div class="section__form">
 
-<div class="section__h4">
-    <h4 class="form__h4">Tu n'es pas convaincu à 100% ?</h4>
-    <h4 class="form__h4">Prends <span class="combi">rendez-vous</span> pour tester un drone !</h4>
-</div>
-
-
-<div class="form__group">
-
-  <div class="form__nom">
-    <label class="form__label" for="name">Nom</label>
-    <input class="form__input" type="text" name="name" id="name">
-  </div>
-
-  <div class="form__prenom">
-    <label class="form__label" for="name">Prénom</label>
-    <input class="form__input" type="text" name="name" id="name">
-  </div>
-
-  <div class="form__email">
-      <label for="email" class="form__label">Adresse Email</label>
-      <input type="email" class="form__input" placeholder="" id="email" required>
+  <div class="section__h4">
+      <h4 class="form__h4">Tu n'es pas convaincu à 100% ?</h4>
+      <h4 class="form__h4">Prends <span class="combi">rendez-vous</span> pour tester un drone !</h4>
   </div>
 
 
-</div>
+  <div class="form__group">
 
-
-<div class="form__send">
-  <div class="form__send2">
-    <label for="text" class="form__label">Des informations supplémentaires?</label>
-  </div>
-    <input type="text" class="form__input send" placeholder="" id="email" required>
-
-    <div class="btn__container">
-        <a class="btn__link link-blue" href="">Valider</a>
+    <div class="form__nom">
+      <label class="form__label" for="name">Nom</label>
+      <input class="form__input" type="text" name="name" id="name">
     </div>
+
+    <div class="form__prenom">
+      <label class="form__label" for="name">Prénom</label>
+      <input class="form__input" type="text" name="name" id="name">
+    </div>
+
+    <div class="form__email">
+        <label for="email" class="form__label">Adresse Email</label>
+        <input type="email" class="form__input" placeholder="" id="email" required>
+    </div>
+
+
+  </div>
+
+
+  <div class="form__send">
+    <div class="form__send2">
+      <label for="text" class="form__label">Des informations supplémentaires?</label>
+    </div>
+      <input type="text" class="form__input send" placeholder="" id="email" required>
+
+      <div class="btn__container">
+          <a class="btn__link link-blue" href="">Valider</a>
+      </div>
+  </div>
+
 </div>
 
+
+    <!-- <footer class="footer">
+      <div class="footer__container">
+        <div class="contact">
+          <h4 class="footer__h4">Pour mieux nous connaître</h4>
+            <ul class="footer__list">
+              <li class="contact__list-item">À propos d'EDWARD</li>
+              <li class="contact__list-item">Carrières</li>
+              <li class="contact__list-item">Ensemble</li>
+            </ul>
+
+        </div>
+
+        <div class="shop__info">
+         <h4 class="footer__h4">Vos services</h4>
+         <ul class="footer__list">
+              <li class="shop__list-item">Expédia par EDWARD</li>
+              <li class="shop__list-item">Faire livrer votre drone</li>
+              <li class="shop__list-item">Cartes de paiement</li>
+              <li class="shop__list-item">Faire livrer votre drone</li>
+            </ul>
+        </div>
+
+        <div class="news">
+         <h4 class="footer__h4">Besoin d'aide?</h4>
+         <ul class="news__list">
+              <li class="news__list-item">Visualiser ou suivre vos commandes</li>
+              <li class="news__list-item">Tarifs et options de livraison</li>
+              <li class="news__list-item">Retours</li>
+            </ul>
+        </div>
+
+        <div class="copyright">
+          Copyright © 2018 notre site. All Rights Reserved.
+          <div class="socialimg">
+            <ul class="social__list">
+              <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/fb.png" alt=""></a></li>
+              <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/twitter.png" alt=""></a></li>
+              <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/snap.png" alt=""></a></li>
+              <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/insta.png" alt=""></a></li>
+            </ul>
+          </div>
+       </div>
+
+      </div>
+
+    </footer> -->
+
+    <div class="footerimg">
+
+    </div>
+
+
+<!--
+
+    <section class="formulaire">
+    <h4 class="formulaire__h4">Venez essayer !</h4>
+
+      <div class="formulaire__container">
+
+
+          <div class="container">
+            <div class="container__nom">
+              <label class="form__label" for="name">Nom</label>
+              <input class="form__input" type="text" name="name" id="name">
+            </div>
+
+            <div class="container__prenom">
+
+              <label class="form__label" for="name">Prénom</label>
+              <input class="form__input" type="text" name="name" id="name">
+
+            </div>
+
+            <div class="container__tel">
+
+              <label class="form__label" for="name">Téléphone</label>
+              <input class="form__input" type="text" name="name" id="name">
+            </div>
+
+            <div class="container__email">
+
+              <label class="form__label" for="name">Email</label>
+              <input class="form__input" type="text" name="name" id="name">
+            </div>
+
+            <div class="form__cont">
+               <input class="form__text" placeholder="Entrer votre message" type="text" name="name" id="name">
+
+            </div>
+
 </div>
 
 
-  <!-- <footer class="footer">
-    <div class="footer__container">
-      <div class="contact">
-        <h4 class="footer__h4">Pour mieux nous connaître</h4>
-          <ul class="footer__list">
-            <li class="contact__list-item">À propos d'EDWARD</li>
-            <li class="contact__list-item">Carrières</li>
-            <li class="contact__list-item">Ensemble</li>
-          </ul>
-
       </div>
+    </section>
 
-      <div class="shop__info">
-       <h4 class="footer__h4">Vos services</h4>
-       <ul class="footer__list">
-            <li class="shop__list-item">Expédia par EDWARD</li>
-            <li class="shop__list-item">Faire livrer votre drone</li>
-            <li class="shop__list-item">Cartes de paiement</li>
-            <li class="shop__list-item">Faire livrer votre drone</li>
-          </ul>
-      </div>
 
-      <div class="news">
-       <h4 class="footer__h4">Besoin d'aide?</h4>
-       <ul class="news__list">
-            <li class="news__list-item">Visualiser ou suivre vos commandes</li>
-            <li class="news__list-item">Tarifs et options de livraison</li>
-            <li class="news__list-item">Retours</li>
-          </ul>
-      </div>
 
-      <div class="copyright">
-        Copyright © 2018 notre site. All Rights Reserved.
-        <div class="socialimg">
+
+
+
+
+    <section class="beforefooter">
+
+        <div class="container__h3">
+          <h3 class="before__h3">NOTRESITE</h3>
+        </div>
+
+        <div class="container__text">
+          <p class="before__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt id unde porro sequi? Nam obcaecati molestiae, soluta debitis, eum laborum architecto quibusdam laudantium quo animi non maiores, corrupti placeat magnam.</p>
+        </div>
+
+
+        <div class="social">
           <ul class="social__list">
             <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/fb.png" alt=""></a></li>
             <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/twitter.png" alt=""></a></li>
@@ -162,18 +239,50 @@
             <li class="social__item"><a class="social__link" href=""><img class="social__img" src="../img/insta.png" alt=""></a></li>
           </ul>
         </div>
-     </div>
+    </section>
 
-    </div>
+    <footer class="footer">
+      <div class="footer__container">
+        <div class="contact">
+          <h4 class="footer__h4">Contact us</h4>
+            <ul class="footer__list">
+              <li class="contact__list-item">Nom du site</li>
+              <li class="contact__list-item">Adresse du site</li>
+              <li class="contact__list-item">Ville du site + Pays</li>
+              <li class="contact__list-item">01 02 03 04 05</li>
+              <li class="contact__list-item">monsite@gmail.com</li>
+            </ul>
 
-  </footer> -->
+        </div>
 
-  <div class="footerimg">
+        <div class="shop__info">
+         <h4 class="footer__h4">Shopping info</h4>
+         <ul class="footer__list">
+              <li class="shop__list-item">Livraisons</li>
+              <li class="shop__list-item">Services</li>
+              <li class="shop__list-item">Retours</li>
+              <li class="shop__list-item">Fabriquants</li>
+              <li class="shop__list-item">Contact us</li>
+            </ul>
+        </div>
 
-  </div>
+        <div class="news">
+         <h4 class="footer__h4">Newsletter</h4>
+          <p class="news__text">Inscrivez-vous à notre newsletter et recevez nos meilleures promotions dans votre boite mail !</p>
 
+          <div class="form__group">
+              <input type="email" class="form__input" placeholder="monemail@gmail.com" id="email" required>
+              <button>GO</button>
+          </div>
+        </div>
 
-  <script src="js/main.js"></script>
-</body>
+        <div class="copyright">
+          Copyright © 2018 notre site. All Rights Reserved.
+       </div>
 
+      </div>
+
+    </footer>
+  -->
+  </body>
 </html>
